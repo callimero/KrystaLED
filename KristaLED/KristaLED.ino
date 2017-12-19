c130d11d26cc89aa17a16caa6eac477225c816c8
callimero <c.wartmann@gmx.net>
2017-12-19 16:39:08 +0100

    POrt Number matches to PCB

--- KristaLED/KristaLED.ino
+++ KristaLED/KristaLED.ino
@@ -7,7 +7,7 @@
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN 0
+#define DATA_PIN 4
 
 // Define the array of leds
 CRGB led[1];
